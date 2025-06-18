84ebdf51687abb0e6a2be51da00e089e7e01bf0c
jihoon kwak <kwakjihoon1001@gmail.com>
2025-06-18 11:04:14 +0900

    fix: 디렉토리 구조 변경 3차..

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,23 @@ const aliases = Object.entries(tsPathsConfig.compilerOptions.paths).map(([key, [
 }));
 
 //* https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   resolve: {
+//     alias: aliases,
+//   },
+// });
+
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: aliases,
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // 기본값이지만 확인 필요
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      ...aliases,
+    },
+  },
+  base: '/', // 👈 이거 빠지면 상대 경로 문제 생김
 });
