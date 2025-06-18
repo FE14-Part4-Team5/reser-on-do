9383cc9031d176894c13f86e10fb9089afa4dfa1
ghdtnals <hongsumin58@gmail.com>
2025-06-18 13:39:28 +0900

    feat: ToastContainer 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
+import ToastContainer from './components/toast/ToastContainer';
+
 import './styles/global.css';
 
 const App = () => {
   return (
     <div>
       <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 };
